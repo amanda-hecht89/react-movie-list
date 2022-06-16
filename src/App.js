a1957abc3b415b63e94908a6a56177d05050667b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-15 18:38:15 -0600

    const

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import './App.css';
 import { useState } from 'react';
 import MovieForm from './Movies/MovieForm';
 import MovieList from './Movies/MovieList';
-import Movies from './Movies/Movie';
+import Movie from './Movies/Movie';
 
 function App() {
+
+  const [allMovies, setAllMovies] = useState([]);
+  const [showMovies, setShowMovies] = useState([]);
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieDirector, setMovieDirector] = useState('');
+  const [movieColor, setMovieColor] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className='add-movie'>
+          <Movie />
+        </div>
+        <div className='movie-filter'>
+          <MovieForm />
+        </div>
+        <div className='movie-list'>
+          <MovieList />
+        </div>
       </header>
     </div>
   );
